9f2fb0fd0290f58a70c3fd01d13761a82e51a8c3
Vinicius Frota <viniciuspxfrota@gmail.com>
2023-02-02 19:23:29 -0300

    update css dinamically

--- src/components/VariationCard/index.tsx
+++ src/components/VariationCard/index.tsx
@@ -1,21 +1,26 @@
 import arrow from '../assets/icons/arrow.svg';
 
 type VariationCardProps = {
-    heading:string;
-    value:string; 
-    color:string;
+    heading: string;
+    value: string; 
+    color: string;
     up: boolean;
 };
 
 export default function VariationCard( { heading, value, color, up } : VariationCardProps ) {
     return (
         <div className="flex items-center">
-            <div className={`mr-3 w-9 h-9 bg-${color}-600 rounded-full flex justify-center ${up ? 'rotate-180':''}`}>
+            <div className={`mr-3 w-9 h-9 bg-${color}-600 rounded-full flex justify-center `} 
+                style= {{
+                    transform: up ? "rotate(180deg)" : "none",
+                    backgroundColor: color
+                }}
+            >
                 <img src={arrow} alt="arrow" className="w-8"/>
             </div>
       
             <div className="flex flex-col items-center">
-                <h1 className={`text-${color}-600`}>{heading}</h1>
+                <h1 style={{color: color}}>{heading}</h1>
                 <p>{value}</p>
             </div>
         </div>
