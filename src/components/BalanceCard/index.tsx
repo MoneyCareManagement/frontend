f3637483caec61acc7e464280f79f6feb502d960
Vinicius Frota <viniciuspxfrota@gmail.com>
2023-02-02 21:03:13 -0300

    add Links

--- src/components/BalanceCard/index.tsx
+++ src/components/BalanceCard/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Link } from "react-router-dom";
 import VariationCard from "../VariationCard";
 
 export default function BalanceCard() {
@@ -9,17 +10,19 @@ export default function BalanceCard() {
 
     console.log(balance);
     return (
-        <section className="w-3/4 bg-purple-800 h-96 rounded-b-3xl flex flex-col items-center justify-around">      
+        <section className="w-3/4 bg-purple-800 h-60 rounded-b-3xl flex flex-col items-center justify-between">      
             <h1>February</h1>
 
-            <section className="flex flex-col items-center">
-                <h1>Accounts balance</h1>
-                <p>{ balance }</p>
+            <section className="w-1/2 h-1/3">
+                <Link to="/accounts" className="flex flex-col items-center hover:bg-purple-900">
+                    <h1>Accounts balance</h1>
+                    <p>{ balance }</p>
+                </Link>
             </section>
 
-            <div className="flex justify-around w-full">
-                <VariationCard heading={"Incomes"} value={income} color={"green"} up={true}/>
-                <VariationCard heading={"Expenses"} value={expense} color={"red"} up={false}/>
+            <div className="flex justify-around w-full h-1/3">
+                <VariationCard heading={"incomes"} value={income} color={"green"} up={true}/>
+                <VariationCard heading={"expenses"} value={expense} color={"red"} up={false}/>
             </div>
         </section>
     );
--- src/components/VariationCard/index.tsx
+++ src/components/VariationCard/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import arrow from '../assets/icons/arrow.svg';
 
 type VariationCardProps = {
@@ -9,20 +10,22 @@ type VariationCardProps = {
 
 export default function VariationCard( { heading, value, color, up } : VariationCardProps ) {
     return (
-        <div className="flex items-center">
-            <div className={`mr-3 w-9 h-9 bg-${color}-600 rounded-full flex justify-center `} 
-                style= {{
-                    transform: up ? "rotate(180deg)" : "none",
-                    backgroundColor: color
-                }}
-            >
-                <img src={arrow} alt="arrow" className="w-8"/>
-            </div>
-      
-            <div className="flex flex-col items-center">
-                <h1 style={{color: color}}>{heading}</h1>
-                <p>{value}</p>
-            </div>
-        </div>
+        <section className='w-1/2'>
+            <Link to={`/${heading}`} className="flex items-center justify-center hover:bg-purple-900 h-full">
+                <div className={`mr-3 w-9 h-9 rounded-full flex justify-center`} 
+                    style= {{
+                        transform: up ? "rotate(180deg)" : "none",
+                        backgroundColor: color
+                    }}
+                >
+                    <img src={arrow} alt="arrow" className="w-8"/>
+                </div>
+    
+                <div className="flex flex-col items-center">
+                    <h1 className='capitalize' style={{color: color}}>{heading}</h1>
+                    <p>{value}</p>
+                </div>
+            </Link>
+        </section>
     );
 };
